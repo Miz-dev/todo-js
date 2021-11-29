a7b470d94495a0a26e53121b74915d924b4f9d3e
shn's works <53104737+shnlog@users.noreply.github.com>
2021-11-29 19:35:14 +0900

    [add]タスクの追加機能(テキスト)実装

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import "./styles.css";
 
-// document.getElementById("app").innerHTML = `
-// <h1>Hello Vanilla!</h1>
-// <div>
-//   We use the same configuration as Parcel to bundle this sandbox, you can find more
-//   info about Parcel
-//   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-// </div>
-// `;
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.querySelector("#add-text").value;
+  document.querySelector("#add-text").value = "";
+
+  // li生成
+  const li = document.createElement("li");
+  li.className = "incomplete-item";
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // pタグ生成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  // divタグの子要素に各要素を設定
+  li.appendChild(div);
+  div.appendChild(p);
+
+  // 未完了の子要素に各要素を設定
+  document.querySelector("#incomplete-list").appendChild(li);
+};
+
+document
+  .querySelector("#add-button")
+  .addEventListener("click", () => onClickAdd());
