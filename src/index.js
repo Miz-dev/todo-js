e49e56b61b84926ced39bb7c19d055e23e00f6c2
shn's works <53104737+shnlog@users.noreply.github.com>
2021-11-30 19:01:50 +0900

    [add]タスクの完了機能実装

--- src/index.js
+++ src/index.js
@@ -21,14 +21,51 @@ const onClickAdd = () => {
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
-    alert("完了");
+    // 押下された完了ボタンの親タグ(div)を未完了リストから削除
+    deleteFromIncompleteList(completeButton.closest(".incomplete-item"));
+
+    // 完了リストに追加する要素
+    const addTarget = completeButton.closest(".incomplete-item");
+
+    // TODO内容テキストを取得
+    const text = addTarget.children[0].firstElementChild.innerText; // 他の指定方法あるはず
+
+    // div以下を初期化
+    addTarget.textContent = null;
+
+    // li生成
+    const li = document.createElement("li");
+    li.className = "incomplete-item";
+
+    // div生成
+    const div = document.createElement("div");
+    div.className = "list-row";
+
+    // pタグ生成
+    const p = document.createElement("p");
+    p.innerText = text;
+
+    // button(戻る)タグ生成
+    const ｂａｃｋButton = document.createElement("button");
+    ｂａｃｋButton.innerText = "戻す";
+
+    // divタグの子要素に各要素を設定
+    addTarget.appendChild(div);
+    div.appendChild(p);
+    div.appendChild(ｂａｃｋButton);
+
+    // 完了の子要素に各要素を設定
+    document.getElementById("complete-list").appendChild(addTarget);
+
+    // console.log(addTarget);
   });
 
   // button(削除)タグ生成
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
-    alert("削除");
+    // 押下された削除ボタンの親タグ(div)を未完了リストから削除
+    deleteFromIncompleteList(deleteButton.closest(".incomplete-item"));
   });
 
   // divタグの子要素に各要素を設定
@@ -39,18 +76,13 @@ const onClickAdd = () => {
 
   // 未完了の子要素に各要素を設定
   document.getElementById("incomplete-list").appendChild(li);
+
+  // 未完了リストから指定の要素を削除
+  const deleteFromIncompleteList = (target) => {
+    document.getElementById("incomplete-list").removeChild(target);
+  };
 };
 
 document
   .getElementById("add-button")
   .addEventListener("click", () => onClickAdd());
-
-// 祖先要素を取得する方法
-```js
-closest(“セレクタ名”)
-```
-
-// 親要素を取得する方法
-```js
-parentNode
-```
